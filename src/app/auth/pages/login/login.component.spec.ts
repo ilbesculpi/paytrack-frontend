99f2965d74732527d20f23e0a8636677ac341344
Ilbert Esculpi <ilbert.esculpi@gmail.com>
2025-03-17 12:39:29 -0400

    login page

--- src/app/auth/pages/login/login.component.spec.ts
+++ src/app/auth/pages/login/login.component.spec.ts
@@ -3,21 +3,22 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
-  let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [LoginComponent]
-    })
-    .compileComponents();
+    let component: LoginComponent;
+    let fixture: ComponentFixture<LoginComponent>;
 
-    fixture = TestBed.createComponent(LoginComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [LoginComponent],
+        }).compileComponents();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        fixture = TestBed.createComponent(LoginComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
 });
--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AbstractControl, NonNullableFormBuilder, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-login',
@@ -6,4 +7,26 @@ import { Component } from '@angular/core';
     styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+
+    form = this.fb.group({
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+
+    get inputEmail(): AbstractControl|null {
+        return this.form.get('email');
+    }
+
+    get inputPassword(): AbstractControl|null {
+        return this.form.get('password');
+    }
+
+    constructor(private fb: NonNullableFormBuilder) {
+    }
+
+    submit() {
+        if (this.form.valid) {
+            console.log(this.form.value);
+        }
+    }
 }
